3b13c3f123f9bee1ee9ab15c1c88fd8cdb7a20f5
Marshal Olimmah <marshalolimmah@gmail.com>
2025-04-07 19:04:30 -0500

    Deleted Simple.java file

--- simple.java
+++
@@ -1,9 +0,0 @@
-// Your first Java code Program
-
-class Simple{
-	public static void main(String[] arg) {
-		System.out.println("Welcome to Git's online Training!!!d! For more information, Visit Edureka.co");
-
-        } 
-
-}
